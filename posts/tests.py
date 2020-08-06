40ad523ba1e30b1c5166098bac96bb2af6a3f742
Vladislav <sidorenkov.slava@gmail.com>
2020-08-06 16:35:36 +0300

    add profile

--- posts/tests.py
+++ posts/tests.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 
 class TestStringMethods(TestCase):
     def test_length(self):
-                self.assertEqual(len('yatube'), 6)
+        self.assertEqual(len('yatube'), 6)
 
     def test_show_msg(self):
-                # действительно ли первый аргумент — True?
-                self.assertTrue(False, msg="Важная проверка на истинность")
+        # действительно ли первый аргумент — True?
+        self.assertTrue(False, msg="Важная проверка на истинность")
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect, render, HttpResponse
 from django.core.paginator import Paginator
+from django.contrib.auth import get_user_model
 
 from .forms import PostForm
 from .models import Group, Post
@@ -19,9 +20,6 @@ def index(request):
 
 
 def group_posts(request, slug):
-    # group = get_object_or_404(Group, slug=slug)
-    # posts = group.posts.all()[:12]
-    # return render(request, 'group.html', {'group': group, 'posts': posts})
     group = get_object_or_404(Group, slug=slug)
     post_list = group.posts.all()
     paginator = Paginator(post_list, 10) 
@@ -44,3 +42,39 @@ def new_post(request):
     else:
         form = PostForm()
     return render(request, 'new_post.html', {'form': form})
+
+def profile(request, username):
+    User = get_user_model()
+    author = get_object_or_404(User, username=username)
+
+    post_list = author.posts.all()
+    paginator = Paginator(post_list, 10)  
+
+    page_number = request.GET.get('page') 
+    page = paginator.get_page(page_number) 
+    return render(request, 'profile.html', {'author': author, 'page': page, 'paginator': paginator})
+    # post_list = Post.objects.all()
+    # paginator = Paginator(post_list, 10)  
+
+    # page_number = request.GET.get('page') 
+    # page = paginator.get_page(page_number) 
+    # return render(
+    #     request,
+    #     'index.html',
+    #     {'page': page, 'paginator': paginator}
+    # )
+ 
+def post_view(request, username, post_id):
+    return HttpResponse(f"post_view {username}, {post_id}")
+
+        # тут тело функции
+        #return render(request, 'post.html', {})
+
+
+def post_edit(request, username, post_id):
+    return HttpResponse(f"post_edit {username}, {post_id}")
+        # тут тело функции. Не забудьте проверить, 
+        # что текущий пользователь — это автор записи.
+        # В качестве шаблона страницы редактирования укажите шаблон создания новой записи
+        # который вы создали раньше (вы могли назвать шаблон иначе)
+        #return render(request, 'post_new.html', {})
