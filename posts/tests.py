a94ec9daf4b22c0d0fea0882b34caa938fcbefc4
Vladislav <sidorenkov.slava@gmail.com>
2020-08-06 12:52:39 +0300

    add tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,1 +1,10 @@
 from django.test import TestCase
+
+
+class TestStringMethods(TestCase):
+    def test_length(self):
+                self.assertEqual(len('yatube'), 6)
+
+    def test_show_msg(self):
+                # действительно ли первый аргумент — True?
+                self.assertTrue(False, msg="Важная проверка на истинность")
