198c57c3e4c91a304fa05dd040ab166bf0789bc3
sidorenkov-v-a <sidorenkov.v.a@yandex.ru>
2020-08-07 11:19:01 +0300

    urls flatpages bugfix

--- yatube/urls.py
+++ yatube/urls.py
@@ -10,14 +10,16 @@ urlpatterns = [
     # регистрация и авторизация
     path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
-    # импорт из приложения posts
-    path('', include('posts.urls')),
 ]
 
 urlpatterns += [
-        path('about-us/', views.flatpage, {'url': '/about-us/'}, name='about'),
-        path('terms/', views.flatpage, {'url': '/terms/'}, name='terms'),
-        path('about-author/', views.flatpage, {'url': '/about-author/'}, name='about-author'),
-        path('about-spec/', views.flatpage, {'url': '/about-spec/'}, name='about-spec'),
+    path('about-us/', views.flatpage, {'url': '/about-us/'}, name='about'),
+    path('terms/', views.flatpage, {'url': '/terms/'}, name='terms'),
+    path('about-author/', views.flatpage, {'url': '/about-author/'}, name='about-author'),
+    path('about-spec/', views.flatpage, {'url': '/about-spec/'}, name='about-spec'),
+]
 
+urlpatterns += [
+    # импорт из приложения posts
+    path('', include('posts.urls')),
 ]
